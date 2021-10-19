6178795ed524ef6ab22b5d82d851d5620b3b4d22
Lautaro Valenzuela <lautarovalenzuela94@gmail.com>
2021-10-19 19:04:58 -0300

    Primera version

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <div id="app" class="container">
+    <div class="jumbotron my-3">
+      <NavBar />
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+import NavBar from './componentes/NavBar'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/componentes/NavBar.vue
@@ -0,0 +1,56 @@
+<template lang="html">
+
+  <section class="src-componentes-nav-bar">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+      <a class="navbar-brand" href="#">Inicio</a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+        </ul>
+      </div>
+
+    </nav>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'src-componentes-nav-bar',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style lang="css">
+  .src-componentes-nav-bar {
+
+  }
+</style>
